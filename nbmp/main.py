8944e998a6203a7184539a224df8c3f8248bb847
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-12 00:25:48 +0300

    Create main.py

---
+++ nbmp/main.py
@@ -0,0 +1,6 @@
+# Copyright Alexander Argentakis (MFDGaming).
+# This file is licensed under the GPLv2 license.
+
+class main:
+    def on_load(self) -> None:
+        self.server.logger.info("Loaded NBMP :)")
