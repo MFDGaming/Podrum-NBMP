771d61231dfe19858059018111fb2ae52b07ea60
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-12 02:18:23 +0300

    Update main.py

--- nbmp/main.py
+++ nbmp/main.py
@@ -1,6 +1,17 @@
 # Copyright Alexander Argentakis (MFDGaming).
 # This file is licensed under the GPLv2 license.
 
+from nbmp.nbs import nbs
+from podrum.protocol.mcbe.packet.level_sound_event_packet import level_sound_event_packet
+import time
+
 class main:
     def on_load(self) -> None:
         self.server.logger.info("Loaded NBMP :)")
+        
+    def play_file(path: str, player: object) -> None:
+        song: object = nbs(path)
+        for i in range(0, song.song_length):
+            if i in song.noteblocks:
+                pass # Sned the packet
+            sleep(1 / (0.05 * song.tempo * 100))
