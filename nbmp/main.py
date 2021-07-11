6c874261dd181f949ce6faa187cf1932879e245a
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-12 02:31:06 +0300

    Update main.py

--- nbmp/main.py
+++ nbmp/main.py
@@ -5,6 +5,26 @@ from nbmp.nbs import nbs
 from podrum.protocol.mcbe.packet.level_sound_event_packet import level_sound_event_packet
 import time
 
+instruments: dict = {
+    "note.harp": 0,
+    "note.bd": 1,
+    "note.snare": 2,
+    "note.hat": 3,
+    "note.bass": 4,
+    "note.bell": 5,
+    "note.flute": 6,
+    "note.chime": 7,
+    "note.guitar": 8,
+    "note.xylophone": 9,
+    "note.iron_xylophone": 10,
+    "note.cow_bell": 11,
+    "note.didgeridoo": 12,
+    "note.bit": 13,
+    "note.banjo": 14,
+    "note.pling": 15,
+    "note.bassattack": 16
+}
+
 class main:
     def on_load(self) -> None:
         self.server.logger.info("Loaded NBMP :)")
@@ -13,5 +33,6 @@ class main:
         song: object = nbs(path)
         for i in range(0, song.song_length):
             if i in song.noteblocks:
-                pass # Sned the packet
+                packet: object = level_sound_event_packet()
+                packet.sound_id
             sleep(1 / (0.05 * song.tempo * 100))
