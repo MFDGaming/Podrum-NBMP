18d67a3b5ee109ebe910dd601c9d5d7e4e6ee181
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-12 16:27:20 +0300

    Update play_command.py

--- nbmp/play_command.py
+++ nbmp/play_command.py
@@ -1,17 +1,5 @@
-#########################################################
-#  ____           _                                     #
-# |  _ \ ___   __| |_ __ _   _ _ __ ___                 #
-# | |_) / _ \ / _` | '__| | | | '_ ` _ \                #
-# |  __/ (_) | (_| | |  | |_| | | | | | |               #
-# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|               #
-#                                                       #
-# Copyright 2021 Podrum Team.                           #
-#                                                       #
-# This file is licensed under the GPL v2.0 license.     #
-# The license file is located in the root directory     #
-# of the source code. If not you may not use this file. #
-#                                                       #
-#########################################################
+# Copyright Alexander Argentakis (MFDGaming).
+# This file is licensed under the GPLv2 license.
 
 from podrum.task.immediate_task import immediate_task
 
@@ -25,6 +13,6 @@ class play_command:
         if sender != self.plugin.server:
             if len(args) >= 1:
                 sender.send_message(f"Playing file")
-                immediate_task(self.plugin.play_file, [args[0], sender]).start()
+                immediate_task(self.plugin.play_file, [" ".join(args), sender]).start()
             else:
                 sender.send_message("/play {file}")
